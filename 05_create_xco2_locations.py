343f9cb06b1f1b24caf4a2895af226341d2c9d54
Hans Chen <contact@hanschen.org>
2026-02-23 22:16:10 +0100

    Fix year and create output dir

--- 05_create_xco2_locations.py
+++ 05_create_xco2_locations.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env -S uv run --script
 """Create files with XCO2 locations."""
 
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import xarray as xr
 
+REF_YEAR = 2021
+OUTPUT_DIR = Path("output/xco2")
 
 ds = xr.load_dataset("output/thinned.nc")
 
@@ -13,13 +17,16 @@ lats = ds_wrf["XLAT"].isel(Time=0)
 lons = ds_wrf["XLONG"].isel(Time=0)
 ds_wrf.close()
 
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+
 for t, time in enumerate(ds.time.values):
     obs = ds["obs_area_frac"][t]
     if not np.any(obs):
         continue
 
     dt = pd.Timestamp(time)
-    out_file = f"output/xco2/{dt:sounding_%Y-%m-%d_%H:%M:%S}.dat"
+    dt = dt.replace(year=REF_YEAR)  # type: ignore
+    out_file = OUTPUT_DIR / f"{dt:sounding_%Y-%m-%d_%H:%M:%S}.dat"
 
     j_indices, i_indices = np.where(obs == 1)
     with open(out_file, "w") as f:
