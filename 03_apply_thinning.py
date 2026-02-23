5c0a868efe0d7a7e3f4513b4dbeac31f3cf065b0
Hans Chen <contact@hanschen.org>
2026-02-23 15:23:21 +0100

    Add script to apply observation thinning

--- 03_apply_thinning.py
+++ 03_apply_thinning.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env -S uv run --script
 """Apply observation thinning to observation coverage."""
+
+import xarray as xr
+
+THINNING = 4
+
+ds = xr.open_dataset("output/cloud_filtered.nc")
+
+mask = xr.zeros_like(ds["obs_area_frac"], dtype=bool)
+mask[:, ::THINNING, ::THINNING] = True
+
+ds_thinned = ds.where(mask, 0)
+
+ds_thinned.to_netcdf("output/thinned.nc")
